3205f7ff2d5d8caa80d093abd03caa9aa5a46421
damian587 <dami.ahumada@duocuc.cl>
2025-06-06 11:02:06 -0400

    Add files via upload

--- creacion_numeros_aleatorios.py
+++ creacion_numeros_aleatorios.py
@@ -2,8 +2,13 @@ import random
 
 
 numeros = []
-for i in range(100):
+for i in range(150):
     numeros.append(random.randint(1,100))
 
 numeros.sort()
 print(numeros)  
+cont = 0
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        cont+=1
+print(f"cantidad de numeros pares : {cont}")
