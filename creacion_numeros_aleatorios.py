24cdea1e0e8813e9ea9115ab58d2c86ac4759f79
damian587 <dami.ahumada@duocuc.cl>
2025-06-06 10:49:06 -0400

    version2
    cambio con una lista con 100 numeros aleatorios

--- creacion_numeros_aleatorios.py
+++ creacion_numeros_aleatorios.py
@@ -1,5 +1,9 @@
 import random
 
-numero =  random.randint(1,100)
 
-print(numero)  
+numeros = []
+for i in range(100):
+    numeros.append(random.randint(1,100))
+
+numeros.sort()
+print(numeros)  
